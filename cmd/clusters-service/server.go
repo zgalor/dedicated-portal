447d7031f3a085d65511579dc1fab2409dfddcaf
Mooli Tayer <mtayer@redhat.com>
2018-07-04 16:30:17 +0300

    extract handlers

--- cmd/clusters-service/server.go
+++ cmd/clusters-service/server.go
@@ -28,81 +28,86 @@ func NewServer(stopCh <-chan struct{}, clusterService ClustersService) *Server {
 
 func (s Server) start() error {
 	r := mux.NewRouter()
-	r.HandleFunc("/clusters", func(w http.ResponseWriter, r *http.Request) {
-		logRequest(r)
-		page, err := getQueryParamInt("page", 0, r)
-		if err != nil {
-			writeJSONResponse(w, http.StatusBadRequest, map[string]string{"error": fmt.Sprintf("%v", err)})
-			return
-		}
-		size, err := getQueryParamInt("size", 100, r)
-		if err != nil {
-			writeJSONResponse(w, http.StatusBadRequest, map[string]string{"error": fmt.Sprintf("%v", err)})
-			return
-		}
-		results, err := s.clusterService.List(ListArguments{Page: page, Size: size})
-		if err != nil {
-			writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": fmt.Sprintf("%v", err)})
-			return
-		}
-		b, err := json.Marshal(results)
-		if err != nil {
-			writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": fmt.Sprintf("%v", err)})
-			return
-		}
-		fmt.Fprintf(w, "%s\n", b)
-	}).Methods("GET")
-	r.HandleFunc("/clusters", func(w http.ResponseWriter, r *http.Request) {
-		bytes, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": fmt.Sprintf("%v", err)})
-			return
-		}
-		var spec Cluster
-		err = json.Unmarshal(bytes, &spec)
-		if err != nil {
-			writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": fmt.Sprintf("%v", err)})
-			return
-		}
-		if spec.UUID != "" {
-			writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": "uuid must be empty"})
-			return
-		}
-		result, err := s.clusterService.Create(spec.Name)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		b, err := json.Marshal(result)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		fmt.Fprintf(w, "%s\n", b)
-	}).Methods("POST")
-
-	r.HandleFunc("/clusters/{uuid}", func(w http.ResponseWriter, r *http.Request) {
-		uuid := mux.Vars(r)["uuid"]
-		if uuid == "" {
-			writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": "no uuid provided"})
-			return
-		}
-		cluster, err := s.clusterService.Get(uuid)
-		if err != nil {
-			writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": fmt.Sprintf("%v", err)})
-			return
-		}
-		b, err := json.Marshal(cluster)
-		if err != nil {
-			writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": fmt.Sprintf("%v", err)})
-			return
-		}
-		fmt.Fprintf(w, "%s\n", b)
-	}).Methods("GET")
+	r.HandleFunc("/clusters", s.listClusters).Methods("GET")
+	r.HandleFunc("/clusters", s.createCluster).Methods("POST")
+	r.HandleFunc("/clusters/{uuid}", s.getCluster).Methods("GET")
 
 	fmt.Println("Listening.")
 	go http.ListenAndServe(":8000", r)
 	return nil
+}
+
+func (s Server) listClusters(w http.ResponseWriter, r *http.Request) {
+	logRequest(r)
+	page, err := getQueryParamInt("page", 0, r)
+	if err != nil {
+		writeJSONResponse(w, http.StatusBadRequest, map[string]string{"error": fmt.Sprintf("%v", err)})
+		return
+	}
+	size, err := getQueryParamInt("size", 100, r)
+	if err != nil {
+		writeJSONResponse(w, http.StatusBadRequest, map[string]string{"error": fmt.Sprintf("%v", err)})
+		return
+	}
+	results, err := s.clusterService.List(ListArguments{Page: page, Size: size})
+	if err != nil {
+		writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": fmt.Sprintf("%v", err)})
+		return
+	}
+	b, err := json.Marshal(results)
+	if err != nil {
+		writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": fmt.Sprintf("%v", err)})
+		return
+	}
+	fmt.Fprintf(w, "%s\n", b)
+}
+
+func (s Server) createCluster(w http.ResponseWriter, r *http.Request) {
+	bytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": fmt.Sprintf("%v", err)})
+		return
+	}
+	var spec Cluster
+	err = json.Unmarshal(bytes, &spec)
+	if err != nil {
+		writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": fmt.Sprintf("%v", err)})
+		return
+	}
+	if spec.UUID != "" {
+		writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": "uuid must be empty"})
+		return
+	}
+	result, err := s.clusterService.Create(spec.Name)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	b, err := json.Marshal(result)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", b)
+}
+
+func (s Server) getCluster(w http.ResponseWriter, r *http.Request) {
+	uuid := mux.Vars(r)["uuid"]
+	if uuid == "" {
+		writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": "no uuid provided"})
+		return
+	}
+	cluster, err := s.clusterService.Get(uuid)
+	if err != nil {
+		writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": fmt.Sprintf("%v", err)})
+		return
+	}
+	b, err := json.Marshal(cluster)
+	if err != nil {
+		writeJSONResponse(w, http.StatusInternalServerError, map[string]string{"error": fmt.Sprintf("%v", err)})
+		return
+	}
+	fmt.Fprintf(w, "%s\n", b)
 }
 
 func getQueryParamInt(param string, defaultValue int, r *http.Request) (value int, err error) {
